1d8ca7ed5e59fc5c72fb9ef5c21f7f3cf7c5daf8
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-05-21 19:27:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,34 +10,32 @@ for(let i=0; i<but.length; i++){
 }
 
 function PlayingAudio(keys){
+	let audio;
   if(keys === "applause"){
-    let audio = new Audio('./sounds/music_1.mp3');
+    audio = new Audio('sounds/music_1.mp3');
     audio.play();
-	  // alert('cli');
   }
   else if(keys === "bao"){
-    let audio = new Audio('sounds/bao.mp3');
+    audio = new Audio('sounds/bao.mp3');
     audio.play();
   }
   else if(keys === "gasp"){
-    let audio = new Audio('sounds/gasp.mp3');
+    audio = new Audio('sounds/gasp.mp3');
     audio.play();
   } 
   else if(keys === "tada"){
-    let audio = new Audio('sounds/tada.mp3');
+    audio = new Audio('sounds/tada.mp3');
     audio.play();
   }
   else if(keys === "victory"){
-    let audio = new Audio('sounds/victory.mp3');
+    audio = new Audio('sounds/victory.mp3');
     audio.play();
   }
   else if(keys === "wrong"){
-    let audio = new Audio('sounds/wrong.mp3');
+    audio = new Audio('sounds/wrong.mp3');
     audio.play();
-  }
-  else if(keys === "stop"){
-    // let audio = new Audio('sounds/stop.wav');
-    audio.pause();
-  }
-   
+  } 
+	document.querySelector('.stop').addEventListener('click', ()=>{
+		audio.pause();
+	})
 }
